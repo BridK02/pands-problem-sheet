e28bc13cb1daa983d25d22044ba7729bfad52fc2
Brid Callaghan <bridcallaghan56@gmail.com>
2022-02-01 19:58:16 +0000

    pands-problem-sheet

--- bmi.py
+++ bmi.py
@@ -3,12 +3,10 @@
 # BMI calculation is weight/(height/100) squared
 # reference mindninjaX/Python-Projects-for-Beginners
 
-weight = float(input ("enter your weight(kg):"))
-print ("weight = {}kg".format(weight))
+weight = float(input ("Enter your weight(kg):"))
 
-height = float(input("enter your height(cm):"))
-print ("height = {}cm".format(weight))
+height = float(input("Enter your height(cm):"))
 
 BMI = weight/(height/100)**2
-
-print (f"BMI is (Kg/m2= {BMI}")
+# reference on how to print superscript in Python https://stackoverflow.com/questions/8651361/how-do-you-print-superscript-in-python
+print (f"The BMI is (kg/m\N{superscript two}) = {BMI}")
