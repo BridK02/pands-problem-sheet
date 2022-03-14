583f0e762612ca57a1aaf90b9b7bd213ba014e9b
Brid Callaghan <bridcallaghan56@gmail.com>
2022-03-14 22:11:55 +0000

    week03

--- week03/secondstring.py
+++ week03/secondstring.py
@@ -7,10 +7,6 @@ from importlib_metadata import Sectioned
 inputString = input ('Please enter a sentence ;')
 
 # from https://realpython.com/reverse-string-python/ will try slice(None, None, -1) function
-# to reverse the sentence
-reversedSentence = inputString[slice(None, None, -1)]
-print ('The reverse of your sentence is {}'.format(reversedSentence))
-
-#how to get every second character [::2]
-secondString = reversedSentence[::2]
-print ('Every second character in reverse of your sentence is {}'.format(secondString))
+# to reverse the sentence and to get every second character [::2]
+reversedSentence = inputString[slice(None, None, -1)][::2]
+print ('Every second character in reverse of your sentence is {}'.format(reversedSentence))
