af90d7ce91be594f29cc631050f39bfab8c18edb
Brid Callaghan <bridcallaghan56@gmail.com>
2022-04-04 14:40:40 +0100

    weekday.py update

--- week05/weekday.py
+++ week05/weekday.py
@@ -1,18 +1,16 @@
 # Write a program that outputs whether or not today is a weekday.
 
 # Author:Brid Kennedy
-# Reference used https://stackoverflow.com/questions/63687176/how-to-check-weekday-using-if-command-in-python-using-datetime
+# Reference used Write a program that outputs whether or not today is a weekday://stackoverflow.com/questions/63687176/how-to-check-weekday-using-if-command-in-python-using-datetime
+# and https://www.w3resource.com/python-exercises/date-time-exercise/python-date-time-exercise-1.php
 # The weekday() method returns the day of the week as an integer where Monday 
 # is indexed as 0 and Sunday is 6.
 
 import datetime
-# mapping the numbers to the days
-week_days = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+weekday = ['Monday', 'Tuesday', 'Wednesday', 'Thursady', 'Friday'] #Weekdays in a list
+now = datetime.datetime.now().strftime('%A') #using datetime module to find current Weekday of the week:
+if now in weekday: # using if and else to print required sentences
+    print("Yes, unfortunately today is a weekday.")
+else:
+    print("It is the weekend, yay!")
 
-now = datetime.datetime.now()
-day = week_days[now.weekday()] # The days of the week in now.weekday() will be numbered 0-6 
-
-if day == 'Tuesday' : #Tuesday is a string ''
-    print ('Yes it is tuesday, unfortunately a weekday')
-else :
-     print ('It is the weekend. Hurray!!')
