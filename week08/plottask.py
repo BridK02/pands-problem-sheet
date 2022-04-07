c17c86e15640676fda6b050601dc69a0e794d4ed
Brid Callaghan <bridcallaghan56@gmail.com>
2022-04-07 23:16:31 +0100

    week08 revisited

--- week08/plottask.py
+++ week08/plottask.py
@@ -4,30 +4,31 @@
 #Author:Brid Kennedy
 
 import numpy as np #import numpy as np instead of simply import numpy saves us some typing in the rest of the code,
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt #likewise for matplotlib function
 
-def f(x): 
-    return x
+def f(x): #define the three functions
+    return x #f=x
 def g(x):
-    return x**2
+    return x**2 #g=x squared
 def h(x):
-    return x**3
+    return x**3 #h=x cubed
 
-x = np.array(range(0,4))
+#setting where x and y data is come from
+x = np.array(range(0,4)) #calling the numpy module, expressly using the range listed in the instructions
 y1 = f(x)
 y2 = g(x)
 y3 = h(x)
 
-
-plt.plot(x,y1)
-plt.plot (x,y2)
-plt.plot (x,y3)
-
+#calling matplotlib to plot the three functions
+plt.plot (x,y1),
+plt.plot (x,y2), 
+plt.plot (x,y3), 
+# adding bits and pieces to the layout of the plot
 plt.title("Week08 Plottask plot")
 plt.xlabel("X")
 plt.ylabel("Y")
 plt.legend()
-
+#plt.line_0("f(x)")
+# save
 plt.savefig('plotask.png')
-
-
+plt.show()
